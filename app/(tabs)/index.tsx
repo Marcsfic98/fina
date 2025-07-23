19dd06e7919cd0c9fc384927e7e50d45014d693b
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2025-07-23 16:07:00 -0300

    valores das box dinamicos

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,6 +6,7 @@ import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 
 
+
 export default function HomeScreen() {
   return (
     
@@ -16,9 +17,9 @@ export default function HomeScreen() {
         style={styles.linearGradientBackground}
       >
         <ScrollView contentContainerStyle={styles.scrollViewContent}>
-        <Box />
-        <Box/>
-        <Box/>
+        <Box name="Entradas" value="1600.00"/>
+        <Box name="Saidas" value="1100.00"/>
+        <Box name="Total" value="1500.00"/>
         <InputAdd/>
         <BoxItens/>
         </ScrollView>
--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -4,11 +4,16 @@ import { Text } from 'react-native';
 import { BlurView } from 'expo-blur';
 import { styles } from './styles';
 
-export function Box() {
+interface BoxProps {
+  name: string;
+  value: string; 
+}
+
+export function Box({name , value}:BoxProps) {
   return (
     <BlurView intensity={50} tint="light" style={styles.container}>
-        <Text style={styles.title}>Entradas</Text>
-        <Text style={styles.value}>R$ 1650.00</Text>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.value}>R$ {value}</Text>
     </BlurView>
   );
 }
